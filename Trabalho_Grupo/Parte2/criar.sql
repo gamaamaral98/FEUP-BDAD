17defa3a6e7efea058a277ca3ea922a9a4bea4f6
Alexandra Mendes <33674643+Gwenevere@users.noreply.github.com>
2018-04-14 20:51:19 +0100

    Started imposing constraints
    Pus isto no sitio errado inicialmente :(

--- Trabalho_Grupo/Parte2/criar.sql
+++ Trabalho_Grupo/Parte2/criar.sql
@@ -10,52 +10,44 @@ DROP TABLE IF EXISTS FUNCIONARIO;
 DROP TABLE IF EXISTS LOJA;
 DROP TABLE IF EXISTS PRODUTO;
 DROP TABLE IF EXISTS TIPOPRODUTO;
-DROP TABLE IF EXISTS DESCONTO;
-DROP TABLE IF EXISTS COMPRA;
-DROP TABLE IF EXISTS FORNECEDOR;
-DROP TABLE IF EXISTS CENTRODISTRIBUICAO;
-DROP TABLE IF EXISTS VENDAS;
 
 CREATE TABLE PESSOA (
   ID INTEGER PRIMARY KEY,
-  Nome TEXT DEFAULT "none", -- verificar se pode ser NULL ou não
-  Idade INTEGER DEFAULT 18 CHECK(Idade > 17), --não podemos ter clientes menores de idade?
-  Morada TEXT(100) DEFAULT "none",
+  Nome TEXT NOT NULL, -- verificar se pode ser NULL ou não
+  Idade INTEGER DEFAULT 18 CHECK(Idade > 17), --não podemos ter clientes menores de idade? --Acho que podes..
+  NIB INTEGER DEFAULT NULL UNIQUE, --  afinal podemos ter um null e um unique na msm cena
+  Morada TEXT(100) NOT NULL, 
   Genero TEXT(1) DEFAULT "F" CHECK( Genero = "F" or Genero = "M"),
   UNIQUE (Nome, Morada)
 );
 
 CREATE TABLE CLIENTE (
   NrCliente INTEGER PRIMARY KEY,
-  Pessoa_ID INTEGER REFERENCES PESSOA,
-  NIB INTEGER UNIQUE,
-  UNIQUE(Pessoa_ID, NIB),
+  Pessoa_ID INTEGER UNIQUE REFERENCES PESSOA,
   UNIQUE(Pessoa_ID, NrCliente)
 );
 
 CREATE TABLE GESTOR (
   ID INTEGER PRIMARY KEY,
-  Pessoa_ID INTEGER REFERENCES PESSOA,
-  NIB INTEGER UNIQUE,
-  Ordenado REAL CHECK (Ordenado > 0),
+  Pessoa_ID INTEGER UNIQUE REFERENCES PESSOA,
+  Ordenado REAL CHECK (Ordenado > 580), --minimum wage is aprox 580
   Contacto TEXT(14) UNIQUE,
   Email TEXT(40) UNIQUE,
-  DataInicio DATE NOT NULL,
-  DataFim DATE NOT NULL,
-  UNIQUE(Pessoa_ID, NIB),
+  DataInicio DATE NOT NULL CHECK(DataInicio > 1910),
+  DataFim DATE NOT NULL CHECK(DataFim > DataInicio),
   UNIQUE(Pessoa_ID, ID)
 );
 
 CREATE TABLE FUNCIONARIO (
   ID INTEGER PRIMARY KEY,
-  Pessoa_ID INTEGER NOT NULL REFERENCES PESSOA,
-  NIB INTEGER UNIQUE,
+  Pessoa_ID INTEGER UNIQUE REFERENCES PESSOA,
+  Ordenado REAL CHECK (Ordenado > 580) NOT NULL,
   RegimeTrabalho TEXT(50) NOT NULL,
-  DuracaoContrato INTEGER NOT NULL CHECK (DuracaoContrato > 0),
-  DataInicio DATE NOT NULL,
+  DuracaoContrato INTEGER CHECK (DuracaoContrato > 0) NOT NULL,
+  DataInicio DATE NOT NULL CHECK(DataInicio > 1910),
   DataFim DATE NOT NULL CHECK (DataFim > DataInicio),
-  UNIQUE(Pessoa_ID, NIB),
-  UNIQUE(Pessoa_ID, ID)
+  UNIQUE(Pessoa_ID, ID),
+ 
 );
 
 CREATE TABLE LOJA (
@@ -68,11 +60,11 @@ CREATE TABLE LOJA (
 );
 
 CREATE TABLE PRODUTO (
-  ID INTEGER,
+  ID INTEGER PRIMARY KEY,
   QuantidadeProduto INTEGER DEFAULT 0 CHECK (QuantidadeProduto > -1),
   Nome TEXT(70) NOT NULL,
   TipoProduto_ID INTEGER NOT NULL REFERENCES TIPOPRODUTO,
-  PRIMARY KEY(ID, Nome)
+  UNIQUE(ID, Nome)
 );
 
 CREATE TABLE TIPOPRODUTO (
@@ -82,44 +74,4 @@ CREATE TABLE TIPOPRODUTO (
   BarCode TEXT(50) NOT NULL,
   DataValidade DATE NOT NULL,
   Fornecedor_ID NOT NULL REFERENCES FORNECEDOR
-);
-
-CREATE TABLE DESCONTO (
-  ID INTEGER PRIMARY KEY,
-  Percentage REAL NOT NULL CHECK(Percentage > 0 and Percentage < 100),
-  DataInicio DATE NOT NULL,
-  DataFim DATE NOT NULL CHECK (DataFim > DataInicio),
-  Loja_Endereço TEXT (70) REFERENCES LOJA,
-  TipoProduto_ID INTEGER REFERENCES TIPOPRODUTO
-);
-
-CREATE TABLE COMPRA (
-  ID INTEGER PRIMARY KEY,
-  PrecoTotal REAL NOT NULL CHECK (PrecoTotal > 0),
-  DataCompra DATE NOT NULL,
-  TipoPagamento TEXT(20) NOT NULL,
-  DataEntrega DATE CHECK (DataCompra > DataEntrega),
-  LocalEntrega TEXT(70),
-  TipoProduto_ID INTEGER NOT NULL REFERENCES TIPOPRODUTO
-);
-
-CREATE TABLE FORNECEDOR (
-  Nome TEXT PRIMARY KEY,
-  Endereço TEXT(70) NOT NULL,
-  Email TEXT(70) NOT NULL,
-  Telefone TEXT(13) NOT NULL
-);
-
-CREATE TABLE CENTRODISTRIBUICAO (
-  ID INTEGER PRIMARY KEY,
-  Localizacao TEXT(70) NOT NULL,
-  Horario TEXT(70) NOT NULL
-);
-
-CREATE TABLE VENDAS (
-  TipoProduto_ID INTEGER NOT NULL,
-  Loja_Endereço TEXT NOT NULL,
-  Lucro REAL NOT NULL,
-  QuantidadeVendida INTEGER NOT NULL (QuantidadeVendida > -1),
-  Data DATE NOT NULL
 );
