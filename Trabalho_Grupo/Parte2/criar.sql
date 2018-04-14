b39538792fb29ed8c12d62e8583875a73b58b078
Duarte Carvalho <up201503661@fe.up.pt>
2018-04-14 16:27:03 +0100

    created CLIENTE table

--- Trabalho_Grupo/Parte2/criar.sql
+++ Trabalho_Grupo/Parte2/criar.sql
@@ -11,5 +11,15 @@ CREATE TABLE PESSOA (
   Nome TEXT DEFAULT "none", -- verificar se pode ser NULL ou não
   Idade INTEGER DEFAULT 18 CHECK(Idade > 17), --não podemos ter clientes menores de idade?
   Morada TEXT(100) DEFAULT "none",
-  Genero TEXT(1) DEFAULT "F" CHECK( Genero = "F" or Genero = "M")
+  Genero TEXT(1) DEFAULT "F" CHECK( Genero = "F" or Genero = "M"),
+  UNIQUE (Nome, Morada)
+);
+
+CREATE TABLE CLIENTE (
+  NrCliente INTEGER PRIMARY KEY,
+  Pessoa_ID INTEGER REFERENCES PESSOA,
+  NIB INTEGER UNIQUE,
+  UNIQUE(Pessoa_ID, NIB),
+  UNIQUE(Pessoa_ID, NrCliente)
+  --,CHECK (Pessoa_ID IN (SELECT ID FROM PESSOA))
 );
