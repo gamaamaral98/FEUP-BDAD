93174f6d08e5ae221dc50793881bf7a0e93ebf4c
Duarte Carvalho <up201503661@fe.up.pt>
2018-05-23 10:35:27 +0100

    Created first querie

---
+++ Trabalho_Grupo/Parte3/queries/int1.sql
@@ -0,0 +1,14 @@
+-- mostra lojas numa Localidade
+
+.mode	columns
+.headers	on
+.nullvalue	NULL
+
+CREATE VIEW  GESTORESBASICINFO AS
+SELECT GESTOR.Pessoa_ID, GESTOR.Contacto, PESSOA.Nome
+FROM GESTOR, PESSOA
+WHERE PESSOA.ID=GESTOR.Pessoa_ID;
+
+SELECT DISTINCT Loja.Endereço, Loja.Telefone, Loja.Horario, GESTORESBASICINFO.Nome, GESTORESBASICINFO.Contacto
+FROM LOJA, GESTOR, GESTORESBASICINFO
+where LOJA.localidade="Porto" AND Loja.Gestor_ID=GESTORESBASICINFO.Pessoa_ID;
