16cc386337ca4d66abb98599fe01d741735e3d13
Duarte Carvalho <up201503661@fe.up.pt>
2018-05-26 02:01:04 +0100

    added gatilho3_adiciona.sql NOT WORKING PROPERLY

---
+++ Trabalho_Grupo/Parte3/triggers/gatilho3_adiciona.sql
@@ -0,0 +1,11 @@
+-- da update do das vendas dum tipo de produto, ainda nao funciona
+CREATE TRIGGER Nova_compra
+AFTER INSERT ON COMPRAEM
+FOR EACH ROW
+BEGIN
+  UPDATE VENDAS
+  SET Lucro = Lucro + (SELECT (QuantidadeProduto * Preco) as "Total"
+                       FROM PRODUTO
+                       WHERE PRODUTO.Compra_ID = New.Compra_ID AND PRODUTO.TipoProduto_ID = VENDAS.TipoProduto_ID)
+  WHERE VENDAS.Loja_Endereço = New.Loja_Endereço;
+END;
