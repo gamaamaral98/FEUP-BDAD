6ae2e229eec9840aeb054c70cecc8ba06253b528
Duarte Carvalho <up201503661@fe.up.pt>
2018-05-27 15:18:48 +0100

    Update gatilho1_verifica.sql

--- Trabalho_Grupo/Parte3/triggers/gatilho1_verifica.sql
+++ Trabalho_Grupo/Parte3/triggers/gatilho1_verifica.sql
@@ -3,3 +3,15 @@
 .nullvalue NULL 
 
 PRAGMA foreign_keys = ON;
+
+select count(*) AS "Nº de pessoas com NIB=20181504"
+FROM PESSOA
+WHERE NIB=20181504;
+
+UPDATE pessoa
+SET nib = 20181504
+WHERE id = 1;
+
+select count(*) AS "Nº de pessoas com NIB=20181504"
+FROM PESSOA
+WHERE NIB=20181504;
