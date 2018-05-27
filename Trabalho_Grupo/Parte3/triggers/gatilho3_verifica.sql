4091951fb48f5f4f33f5159f4da72e3159363886
Duarte Carvalho <up201503661@fe.up.pt>
2018-05-27 16:41:04 +0100

    created gatilho3_verifica

--- Trabalho_Grupo/Parte3/triggers/gatilho3_verifica.sql
+++ Trabalho_Grupo/Parte3/triggers/gatilho3_verifica.sql
@@ -1,5 +1,14 @@
 .mode columns
 .header on
-.nullvalue NULL 
+.nullvalue NULL
 
 PRAGMA foreign_keys = ON;
+
+SELECT COUNT(*) AS "Nº de trabalhadores"
+FROM TRABALHAEM;
+
+INSERT INTO TRABALHAEM VALUES(1, "DASDA");
+INSERT INTO TRABALHAEM VALUES(1231213123, "Rua da Graça, 1170-166");
+
+SELECT COUNT(*) AS "Nº de trabalhadores"
+FROM TRABALHAEM;
