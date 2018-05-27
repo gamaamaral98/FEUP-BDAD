9e5b69a24a9aeb3a629bda8fa55dcca6e3ea7d30
Duarte Carvalho <up201503661@fe.up.pt>
2018-05-27 15:20:26 +0100

    Update gatilho2_remove.sql

--- Trabalho_Grupo/Parte3/triggers/gatilho2_remove.sql
+++ Trabalho_Grupo/Parte3/triggers/gatilho2_remove.sql
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 .mode columns
 .header on
 .nullvalue NULL
@@ -6,7 +5,3 @@
 PRAGMA foreign_keys = ON;
 
 DROP TRIGGER IF EXISTS Produtos_Eliminacao;
-
-=======
-DROP TRIGGER Produtos_Eiminacao;
->>>>>>> bd4dc90f5bfe10546500e5cbb902691703abda10
